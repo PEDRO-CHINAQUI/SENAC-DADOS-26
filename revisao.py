9f90facc1fc33a9170b29737416e0481a749ed17
Pedro Chinaqui <chinaqui.pedro@gmail.com>
2026-03-09 21:11:53 -0300

    primeiro commit

---
+++ revisao.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+dados = 'https://www.ispdados.rj.gov.br/Arquivos/BaseDPEvolucaoMensalCisp.csv'
+df = pd.read_csv(dados, encoding= 'latin1', sep=';') 
+print(df.head(10)) #imprime as primeiras linhas
+df_furto_celular_cisp = df.groupby('cisp')['furto_celular'].sum().reset_index() 
+print(df_furto_celular_cisp)
+
+
